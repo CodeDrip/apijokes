d0364d96aa18f20c107f9aba1b17c66f27d7ee77
CodeDrip <dhaesha@gmail.com>
2019-12-12 19:00:03 -0500

    has jokes going to state. they are not staying there after componentDidMount

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Joke from "./Joke.js"  
 
-
+//grab the joke from api call + store it in a variable
+//call the .joke to show in jsc
+//push the .joke object into an array
 
 
 
@@ -12,7 +14,9 @@ class App extends Component {
     constructor() {
       super()
       this.state = {
-        apiData: []
+         apiData: [],
+        // jokeBag: [],
+        jokeCatch: []
       }
       this.handleClick = this.handleClick.bind(this) 
     }
@@ -25,12 +29,17 @@ class App extends Component {
       .then(res => {
   
         this.setState({ apiData: res})
-        console.log(this.state.apiData.joke)    
+        //add this.var
+        this.jokePitch = this.state.apiData.joke
+        //it logs ok
+        console.log(this.jokePitch)  
+        //get pitch in state
+        this.setState({ jokeCatch: this.jokePitch})  
       })
     }
 
      handleClick() {
-      console.log(`We Got Action`);
+      console.log(this.state.jokeCatch);
       this.componentDidMount();
     }
 
@@ -60,6 +69,116 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import './App.css';
+// import Joke from "./Joke.js"  
+
+
+
+
+
+
+// class App extends Component {
+//     //adding bc we changin the state
+//     constructor() {
+//       super()
+//       this.state = {
+//         apiData: []
+//       }
+//       this.handleRandomClick = this.handleRandomClick.bind(this) 
+//     }
+    
+//     componentDidMount() {
+//       fetch(' https://icanhazdadjoke.com/', {
+//         headers: { Accept: "application/json"}
+//       })
+//       .then(res => res.json())
+//       .then(res => {
+  
+//         this.setState({ apiData: res})
+//         console.log(this.state.apiData.joke)    
+//       })
+//     }
+
+//      handleRandomClick() {
+//       console.log(`We Got Action`);
+//       this.componentDidMount();
+//     }
+    
+
+//   render() {
+    
+//     let actualJoke = this.state.apiData.joke
+    
+    
+      
+
+
+    
+//     return (
+//       <div>
+//         <header className="App-header">
+//           <h1 className="App-title">DAD JOKIE JOKES</h1>
+//         </header>
+//         <main>
+//           <article className="joke-container">
+//             <Joke info = {actualJoke}/>
+//           </article>
+//           <button onClick={this.handleRandonClick}>New Joke</button>
+//         </main>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
 
 
 
